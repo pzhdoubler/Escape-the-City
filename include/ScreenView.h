4623b67fe3b26030e752e7a3b4b4d3c5621ddb9c
Luke Dyer <ltdyer@email.wm.edu>
2019-10-09 22:27:36 -0400

    Added a small amount of skeleton code to ScreenView.h

--- include/ScreenView.h
+++ include/ScreenView.h
@@ -5,7 +5,23 @@
 #ifndef CSCI437_SCREENVIEW_H
 #define CSCI437_SCREENVIEW_H
 
+
+
 class ScreenView {
+
+private:
+
+public:
+    //constructor
+    ScreenView();
+
+    //the controller will be reading in input? Or does screenView need a keyboardInput function like in pong?
+
+    void update(sf::RenderWindow& mainWindow, LogicView& logicView);
+
+    //How will the debug folder work for this? I tried making a Debug folder and running cmake .. and it failed
+    //Also, why won't sf work?
+
 
 };
 
---
+++ src/ScreenView.cpp
@@ -0,0 +1,4 @@
+//
+// Created by 8luke on 10/9/2019.
+//
+
