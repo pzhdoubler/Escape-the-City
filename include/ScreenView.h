f404b167045d4cdb1e8dac1935998a44f4a51a59
Luke Dyer <ltdyer@email.wm.edu>
2019-10-09 22:39:20 -0400

    Added a few more notes pertaining to ScreenView update's purpose

--- include/ScreenView.h
+++ include/ScreenView.h
@@ -17,10 +17,15 @@ public:
 
     //the controller will be reading in input? Or does screenView need a keyboardInput function like in pong?
 
+
+    //ScreenView doesn't need direct references to the character's coordinates, we can just ask for those
+    //from the logic which will hold character instances.
+    //this update should get those coordinates and then draw the main window based on the new coordinates
+    //will also need to draw enemies, barriers, items, other classes using the GameElements interface
     void update(sf::RenderWindow& mainWindow, LogicView& logicView);
 
-    //How will the debug folder work for this? I tried making a Debug folder and running cmake .. and it failed
-    //Also, why won't sf work?
+
+    //is y'all's  namespace sf also not working?
 
 
 };
