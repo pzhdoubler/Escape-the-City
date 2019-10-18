94314fb8b3ffaf76d8b178e3cd5e43be0f063221
Katelyn Etemad <katelynme@gmail.com>
2019-10-18 13:40:30 -0400

    get spawn point method skeletons added

--- src/LevelReader.cpp
+++ src/LevelReader.cpp
@@ -1,11 +1,27 @@
 #include <LevelReader.h>
 
-sf::VertexArray vertices;
+LevelState currentLevelState;
+
+sf::Vector2f fastSpawnPt;
+sf::Vector2f jumpSpawnpt;
 
 LevelReader(){
 
 }
 
-void loadMap(const std::string&){
+void LevelReader::loadMap(const std::string&){
 
+}
+
+void LevelReader::setFastSpawnPt(){
+
+}
+
+void LevelReader::setJumpSpawnPt(){
+
+}
+
+LevelState LevelReader::createLevelState(){
+
+	return currentLevelState;
 }
--- src/LevelState.cpp
+++ src/LevelState.cpp
@@ -1,2 +1,9 @@
 #include <LevelState.h>
 
+sf::Vector2f LevelState::getFastSpawnPt(){
+	return fastSpawnPt;
+}
+
+sf::Vector2f LevelState::getJumpSpawnPt(){
+	return jumpSpawnpt;
+}
