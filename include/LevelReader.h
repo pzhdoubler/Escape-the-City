8fb51afdb3d2b4d37d4f65da25d84080d453a3cb
Katelyn Etemad <katelynme@gmail.com>
2019-10-17 23:29:23 -0400

    Modified to include basic constructors and comments

--- include/LevelReader.h
+++ include/LevelReader.h
@@ -1,8 +1,14 @@
 #ifndef _LEVELREADER_H_
 #define _LEVELREADER_H_
 
-class LevelReader{
+//LevelReader takes input from a text file and interprets it into arguments to pass
+//to the LevelState constructor
 
+class LevelReader{
+	public:
+		LevelReader();
+
+	private:
 }
 
 #endif //_LEVELREADER_H_
--- include/LevelState.h
+++ include/LevelState.h
@@ -1,8 +1,20 @@
 #ifndef _LEVELSTATE_H_
 #define _LEVELSTATE_H_
 
-class LevelState{
+//LevelState takes input from LevelReader and builds the level itself
 
+class LevelState{
+	public:
+		//Constructor
+		//LevelReader interprets text files to pass into the LevelState constructor
+		//Default constructor
+		LevelState();
+
+		//updates background tiles and static objects
+		LevelState updateLevelState();
+
+
+	private:
 }
 
 #endif //_LEVELSTATE_H_
