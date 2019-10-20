192ac23fae151a899c256d437d66b11966d3de61
Luke Dyer <ltdyer@email.wm.edu>
2019-10-20 15:13:21 -0400

    ScreenView now uses the homemade draw method + mainWindow ref

--- src/ScreenView.cpp
+++ src/ScreenView.cpp
@@ -16,6 +16,11 @@ void ScreenView::update(sf::RenderWindow &mainWindow, GameLogic &gameLogic) {
 
     //goes into the drawables vector at jumpman or fastman's location and retrieves a rectangleShape obj to be drawn
     //hopefully the rectangleShapes should have their position, velocity, etc updated in the logic
-    mainWindow.draw(gameLogic.getDrawables().at(0).fast_man);
-    mainWindow.draw(gameLogic.getDrawables().at(1).jump_man);
+//    mainWindow.draw(gameLogic.getDrawables().at(0).fast_man);
+//    mainWindow.draw(gameLogic.getDrawables().at(1).jump_man);
+
+    //the PlayerCharacter objects in the vector each have their own draw method so we can just call that and send in our
+    //mainWindow reference
+    gameLogic.getDrawables().at(0).draw(mainWindow);
+    gameLogic.getDrawables().at(1).draw(mainWindow);
 }
