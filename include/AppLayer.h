34b050b5916495106ae4645e6421b864ae246649
Luke Dyer <ltdyer@email.wm.edu>
2019-11-15 14:12:17 -0500

    added buttons to appLayer for menu transitions

--- include/AppLayer.h
+++ include/AppLayer.h
@@ -16,28 +16,40 @@ private:
     sf::Text hud;
     sf::Font font;
     sf::String mainMenuString = "This is the Main Menu\n "
-                                "Press O for options or L for Level Select";
+                                "Press a button below to continue";
     sf::String optionMenuString = "This is the Option Screen\n"
-                                  "Press B to go back";
+                                  "Press Back to go back";
     sf::String pauseMenuString = "Paused\n "
-                                 "Press SpaceBar to go back\n"
+                                 "Press Back to go back\n"
                                  "Or press a button below to go to Main Menu";
     sf::String levelSelectMenuString = "This is the LevelSelect Screen\n"
                                        "Press a button for a level\n"
-                                       "Or Press B to go back";
+                                       "Or Press Back to go back";
     int characterSize = 28;
     float stringXPos = 0;
     float stringYPos = 0;
+
+    float optionMenuButtonXPos = 400.0f;
+    float optionMenuButtonYPos = 300.0f;
+
+    float levelSelectMenuButtonXPos = 200.0f;
+    float levelSelectMenuButtonYPos = 300.0f;
+
+    float playGameButtonXPos = 300.0f;
+    float playGameButtonYPos = 300.0f;
 
     float button1PauseMenuXPos = 300.0f;
     float button1PauseMenuYPos = 400.0f;
 
     float button1LevelSelectXPos = 200.0f;
-    float button1LevelSelectYPos = 300.0f;
+    float button1LevelSelectYPos = 400.0f;
     float button2LevelSelectXPos = 300.0f;
-    float button2LevelSelectYPos = 300.0f;
+    float button2LevelSelectYPos = 400.0f;
     float button3LevelSelectXPos = 400.0f;
-    float button3LevelSelectYPos = 300.0f;
+    float button3LevelSelectYPos = 400.0f;
+
+    float backButtonXPos = 700.0f;
+    float backButtonYPos = 0.0f;
 
     bool optionMenu(sf::RenderWindow& App);
     bool optionMenuOpen = false;
@@ -61,6 +73,10 @@ private:
     int levelSelectG = 211;
     int levelSelectB = 128;
 
+
+//    sf::Sprite operator == (sf::Sprite const &sprite) {
+//
+//    }
 
 
 
