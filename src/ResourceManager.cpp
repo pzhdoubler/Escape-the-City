85f8e914d8e27f4c54843c9177c40316ea919866
Katelyn Etemad <katelynme@gmail.com>
2019-11-15 12:41:14 -0500

    ResourceManager added

--- src/ResourceManager.cpp
+++ src/ResourceManager.cpp
@@ -1,1 +1,16 @@
-#include <LevelReader.h>
+#include <ResourceManager.h>
+
+ResourceManager::ResourceManager(){
+	//this->spriteSheet.loadFromFile("SPRITESHEET.PNG");
+	this->texture.create(20, 20);
+}
+
+const sf::Texture& ResourceManager::getTexture(int tileID){
+	this->tileID = tileID;
+	//if statements checking the tile id and the corresponding location
+	//on the sprite sheet (specified by rectX and rectY)
+	/*if(tileID == 0){
+		this->texture.update(spriteSheet, rectX, rectY);
+	}*/
+	return texture;
+}
