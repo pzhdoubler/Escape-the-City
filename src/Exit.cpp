eed338e5759ea290f5597d1461af126965873296
Henry Rosalsky <hsrosalsky@email.wm.edu>
2019-11-15 19:09:33 -0500

    Exit h and cpp added

---
+++ src/Exit.cpp
@@ -0,0 +1,49 @@
+#include "Exit.h"
+#include <SFML/Graphics.hpp>
+
+Exit::Exit(){
+  fast_exit = false;
+  jump_exit = true;
+
+  exit.loadFromFile("..\\resources\\Exit.png");
+}
+
+
+
+void Exit::draw(sf::RenderWindow& window){
+  sf::Sprite e;
+  e.setTexture(exit);
+  e.setPosition(pos);
+  window.draw(e);
+}
+
+
+void Exit::setPos(sf::Vector2f& pos){
+  this->pos.x = pos.x;
+  this->pos.y = pos.y;
+}
+
+
+
+sf::Vector2f Exit::getPos(){
+  return pos;
+}
+
+bool Exit::levelEnd(){
+  if(fast_exit && jump_exit){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+
+
+void Exit::PlayerContact(PlayerChar &player,int id){
+  if(player.getType() == true){
+    fast_exit = true;
+  }
+  if(player.getType() == false){
+    jump_exit = true;
+  }
+}
