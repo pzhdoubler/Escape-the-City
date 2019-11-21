f9a0963215142e6cfb96e007823a4db88fc0accc
Katelyn Etemad <katelynme@gmail.com>
2019-11-21 11:02:06 -0500

    ResourceManager textures updated

--- src/ResourceManager.cpp
+++ src/ResourceManager.cpp
@@ -1,7 +1,7 @@
 #include <ResourceManager.h>
 
 ResourceManager::ResourceManager(){
-	//this->spriteSheet.loadFromFile("SPRITESHEET.PNG");
+	this->spriteSheet.loadFromFile("..\\resources\\sprite_sheet");
 	this->texture.create(20, 20);
 }
 
@@ -9,8 +9,49 @@ const sf::Texture& ResourceManager::getTexture(int tileID){
 	this->tileID = tileID;
 	//if statements checking the tile id and the corresponding location
 	//on the sprite sheet (specified by rectX and rectY)
-	/*if(tileID == 0){
+	int rectX;
+	int rectY;
+	//Barrier tile
+	if(tileID == 1){
+		rectX = 0;
+		rectY = 0;
 		this->texture.update(spriteSheet, rectX, rectY);
-	}*/
+	}
+	//jump man
+	else if(tileID == 2){
+		rectX = 40;
+		rectY = 20;
+		this->texture.update(spriteSheet, rectX, rectY);
+	}
+	//fast man
+	else if(tileID == 2){
+		rectX = 60;
+		rectY = 20;
+		this->texture.update(spriteSheet, rectX, rectY);
+	}
+	//button on
+	else if(tileID == 2){
+		rectX = 80;
+		rectY = 0;
+		this->texture.update(spriteSheet, rectX, rectY);
+	}
+	//button off
+	else if(tileID == 2){
+		rectX = 100;
+		rectY = 0;
+		this->texture.update(spriteSheet, rectX, rectY);
+	}
+	//door
+	else if(tileID == 2){
+		rectX = 20;
+		rectY = 20;
+		this->texture.update(spriteSheet, rectX, rectY);
+	}
+	//hazard (spike)
+	else if(tileID == 2){
+		rectX = 0;
+		rectY = 20;
+		this->texture.update(spriteSheet, rectX, rectY);
+	}
 	return texture;
 }
