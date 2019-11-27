d67dd3dbe810487fa8b8cbb58fa90df7606da8c9
Zack H'Doubler <pzhdoubler@email.wm.edu>
2019-11-27 00:57:35 -0500

    added some levels and changed speed item a bit

---
+++ levels/tile_map_36_36.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.0" name="tile_map_36_36" tilewidth="20" tileheight="20" tilecount="36" columns="6">
+ <image source="tile_map_36_36.jpg" width="120" height="120"/>
+</tileset>
--- src/PlayerChar.cpp
+++ src/PlayerChar.cpp
@@ -118,19 +118,19 @@ void PlayerChar::useItem(){
 }
 if(player && (this->powerUp == 3 ||this->powerUp == 4)){
   if(this->vel.x<0){
-    this->vel.x = -600;
+    this->vel.x = -1000;
   }
   else{
-    this->vel.x = 600;
+    this->vel.x = 1000;
   }
   this->powerUp = 0;
 }
 if(player == false && (this->powerUp == 3 ||this->powerUp == 4)){
   if(this->vel.x<0){
-    this->vel.x = -600;
+    this->vel.x = -1000;
   }
   else{
-    this->vel.x = 600;
+    this->vel.x = 1000;
   }
 this->powerUp = 0;
 }
